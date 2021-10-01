281710496f7b9f0b729ce85964a218069b4a0fe5
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2021-10-01 19:28:50 +0300

    added  comment form component

--- src/components/Meme.vue
+++ src/components/Meme.vue
@@ -47,58 +47,40 @@
                                 <a href="#" class="block ml-auto py-2 px-4 hover:bg-indigo-300 rounded-md border border-gray-900 font-medium">Donate 1 Ⓝ</a>
                             </div>
                             <hr class="mt-6 bg-gray-200">
-                            <div class="mt-6">
-                                <h4 class="text-lg text-gray-800 font-medium">Comment</h4>
-                                <form action="" class="grid grid-cols-6 space-x-4 mt-2">
-                                    <input type="text" placeholder="Write comment" class="col-span-4 py-2 pl-6 border border-gray-500 rounded-md w-full outline-none">
-                                    <a href="#" class="col-span-2 block bg-indigo-500 hover:bg-indigo-600 py-2 px-4 text-white text-center font-medium rounded-md">Send</a>
-                                </form>
-                            </div>
-                            <div class="mt-6 px-6 py-4 bg-indigo-200 rounded-md">
-                                <div class="flex">
-                                    <h3 class="text-gray-600 font-light">nazarh2onyzhynk.near</h3>
-                                    <p class="ml-auto text-gray-600 font-light">Aug 13th 2021</p>
-                                </div>
-                                <p class="text-lg text-gray-900 font-medium">
-                                    El hopaness, nice XD
-                                </p>
-                                <div class="flex items-center mt-4">
-                                    <a href="#" class="flex items-center text-gray-900 font-medium hover:underline">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
-                                        </svg>
-                                        Reply
-                                    </a>
-                                    <div class="ml-auto">
-                                        <ul class="flex ml-6 space-x-4">
-                                            <li class="">
-                                                <a href="#" class="text-gray-600 hover:text-indigo-700">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5" />
-                                                    </svg>
-                                                </a>
-                                            </li>
-                                            <li class="">
-                                                <a href="#" class="text-indigo-700 hover:text-indigo-700">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-                                                    </svg>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
+                            <comment-form/>
                         </div>
                     </div>
 </template>
 
 <script>
-export default {
+import { format, fromUnixTime } from "date-fns";
+import CommentForm from '@/components/CommentForm.vue';
 
+
+export default {
+    components: {
+        CommentForm
+    },
+    props: {
+        memes: {
+            type: Array,
+            required: true
+        },
+        addComment: {
+            type: Function,
+            required: true
+        },
+        donate: {
+            type: Function,
+            required: true
+        },
+        vote: {
+            type: Function,
+            required: true
+        }
+    },
+    setup() {
+        return { format, fromUnixTime }
+    }
 }
 </script>
-
-<style>
-
-</style>
