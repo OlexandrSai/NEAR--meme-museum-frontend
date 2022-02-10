425a3453bebe43a895a74de4ad72c37c0cac7a87
mihail.shevchuk <mihail.s@hubx.capital>
2022-02-10 19:47:44 +0200

    NG | Add Meme Form

--- src/app/components/add-meme-form/add-meme-form.component.ts
+++ src/app/components/add-meme-form/add-meme-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MemeService} from "../../services/meme.service";
 
 @Component({
   selector: 'app-add-meme-form',
@@ -6,23 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-meme-form.component.css']
 })
 export class AddMemeFormComponent implements OnInit {
+  public category = 0;
   public formFields = [
     {
       label: "Meme Name",
       id: "meme",
+      value: ''
     },
     {
       label: "Title",
       id: "title",
+      value: ''
     },
     {
       label: "9gag Link",
       id: "data",
+      value: ''
     },
   ];
-  constructor() { }
+  constructor(public memeService: MemeService) { }
 
   ngOnInit(): void {
   }
 
+  submit() {
+    const values = this.formFields.map(e => e.value);
+    const payload = {
+      meme: values[0],
+      title: values[1],
+      data: values[2],
+      category: this.category
+    }
+
+    this.memeService.addMeme(payload);
+  }
 }
